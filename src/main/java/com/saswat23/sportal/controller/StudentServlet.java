3406998a0df816cdb064854409e1a32eaf2c584c
saswat-23 <saswat23@gmail.com>
2024-05-23 21:26:52 +0530

    [SERVLET-APP] Initial Version

---
+++ src/main/java/com/saswat23/sportal/controller/StudentServlet.java
@@ -0,0 +1,21 @@
+package com.saswat23.sportal.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class StudentServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("<h1>1st Servlet App</h1>");
+	}
+	
+	public String showDemoPage() {
+		return "views/index.jsp";
+	}
+	
+}
