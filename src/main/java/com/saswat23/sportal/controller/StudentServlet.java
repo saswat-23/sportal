0f4ae2d5257566e0a0470dc25d77a6163fbb9bb4
saswat-23 <saswat23@gmail.com>
2024-05-23 21:34:18 +0530

    [SERVLET-APP] Annotated with @WebServlet

--- src/main/java/com/saswat23/sportal/controller/StudentServlet.java
+++ src/main/java/com/saswat23/sportal/controller/StudentServlet.java
@@ -3,10 +3,12 @@ package com.saswat23.sportal.controller;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/")
 public class StudentServlet extends HttpServlet {
 	
 	@Override
