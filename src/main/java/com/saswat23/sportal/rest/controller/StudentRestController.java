2e0245e021b968b1b2117d50c7e3a66be284b0ab
saswat-23 <saswat23@gmail.com>
2024-06-09 16:21:16 +0530

    [SPRING-MVC-REST] Controller updated to RestController

--- src/main/java/com/saswat23/sportal/rest/controller/StudentRestController.java
+++ src/main/java/com/saswat23/sportal/rest/controller/StudentRestController.java
@@ -3,52 +3,45 @@ package com.saswat23.sportal.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.saswat23.sportal.model.Student;
 import com.saswat23.sportal.service.StudentService;
 
-@Controller
+@RestController
 public class StudentRestController {
 	
 	@Autowired
 	private StudentService service;
 	
 	@RequestMapping(path = {"/", "/students"}, method = RequestMethod.GET)
-	@ResponseBody
 	public List<Student> getAllStudents() {
 		List<Student> studs = service.getAllStudents();
 		return studs;
 	}
 	
 	@RequestMapping(path = "/student", method = RequestMethod.POST)
-	@ResponseBody
 	public void addStudent(@RequestBody Student stud) {
 		service.addStudent(stud);
 	}
 	
 	
 	@RequestMapping(path = "/student/{studentId}", method =  RequestMethod.GET)
-	@ResponseBody
 	public Student getStudent(@PathVariable("studentId") String studentId) {
 		return service.getStudent(studentId);
 	}
 	
 	
 	@RequestMapping(path = "/student", method = RequestMethod.PUT)
-	@ResponseBody
 	public Student updateStudent(@RequestBody Student student) {
 		return service.updateStudent(student);
 	}
 	
 	@RequestMapping(path = "/student/{studentId}", method =  RequestMethod.DELETE)
-	@ResponseBody
 	public String deleteStudent(@PathVariable("studentId") String studentId) {
 		Student stud = new Student();
 		stud.setUserid(studentId);
@@ -56,7 +49,6 @@ public class StudentRestController {
 	}
 	
 	@RequestMapping(path = "/deleteAll", method = RequestMethod.DELETE)
-	@ResponseBody
 	public String deleteAllStudents() {
 		if(service.deleteAllStudents()) {
 			return "All student details deleted successfully!";
@@ -66,7 +58,6 @@ public class StudentRestController {
 	
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/restoreData")
-	@ResponseBody
 	public List<Student> restoreAllStudents() {
 		return service.restoreStudentData();
 	}
